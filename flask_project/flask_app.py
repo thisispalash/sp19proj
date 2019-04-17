226de00d2ca6bf10929595c6c76fffc03499aef1
ahaan15 <ahaangoswami@gmail.com>
2019-04-17 10:53:27 +0530

    Trying to add flask app

---
+++ flask_project/flask_app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from form import register#login
+app = Flask(__name__)
+
+app.config['SECRETE_KEY']= '4fa371198aeace66523b19ddc73fc6e7'
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "<h1>Home Page! (Blog)</h1>"
+
+@app.route('/about')
+def about():
+    #return "<h1>Team:</h1>"
+    return render_template('index.html')
+
+@app.route('/auth')
+def auth():
+    return "<h1>Login & Management</h1>"
+
+@app.route('/register')
+def register():
+    form=register()
+    return render_template('register.html',title= 'Register',form=form')
+
+# @app.route('/login')
+# def login():
+#     form=login()
+#     return render_template('login.html',title= 'Login',form=form')
+
+@app.route('/browse')
+def browse():
+    return render_template('navbar.html')
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
+    
