48c7445b22a54d4d7c35db6ce3acfa2c08b2a56d
Palash A <palash.agarwal.96@gmail.com>
2019-04-22 03:58:06 +0530

    part 2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, flash, url_for, redirect
+from flask import Flask, render_template, request, flash, url_for, redirect, session, abort
 from form import register_form#login_form
 
 from helper import app
@@ -9,16 +9,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 from model import User,Zite
 
-
 @app.route('/')
 @app.route('/home')
 @app.route('/index')
 def home():
-    return render_template('home.html', blog=True)
+    return render_template('home.html', blog=True, session=session)
 
 @app.route('/about')
 def about():
-    return render_template('home.html', blog=False)
+    return render_template('home.html', blog=False, session=session)
 
 @app.route('/login')
 @app.route('/auth')
@@ -30,14 +29,21 @@ def login():
     elif method == 'POST':
         return render_template('index.html')
 
-@app.route('/register',methods=['GET','POST'])
+@app.route('/register')
 def register():
-    form=register_form()
+    if request.method == 'GET':
+        return render_template(
+            'index.html',
+            render='register.html',
+            form=register_form(),
+            session=session
+            )
+    
     if form.validate_on_submit():
         #hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         flash(f'Account created for {form.username.data}!','success')
         return redirect(url_for('login'))
-    return render_template('register.html',title= 'Register',form=form)
+    return render_template('register.html',title='Register',form=form)
 
 @app.route('/browse')
 def browse():
--- form.py
+++ form.py
@@ -4,10 +4,9 @@ from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 from model import User
  
 class register_form(FlaskForm):
-    username = StringField('Username: ',validators=[DataRequired(), Length(min=2, max=20)])
-    password= PasswordField('Password: ',validators=[ DataRequired()])
-    confirm_password= PasswordField('Confirm Password: ',validators=[ DataRequired(),EqualTo('password')])
-    submit= SubmitField('Sign up') 
+    name = StringField('Name: ', validators=[DataRequired(), Length(min=3,max=25)])
+    username = StringField('Username: ', validators=[DataRequired(), Length(min=2, max=20)])
+    submit= SubmitField('Join')
 
     def validate_username(self,username):
         user = User.query.filter_by(username=username.data).first()
@@ -16,6 +15,6 @@ class register_form(FlaskForm):
 
 class login_form(FlaskForm):
     username= StringField('Username: ',validators=[DataRequired(),Length(min=2,max=20 )])
-    password= PasswordField('Password: ',validators=[DataRequired()])
+    password= PasswordField('Private Key: ',validators=[DataRequired()])
     remember= BooleanField('Remember me')
-    submit=SubmitField('Log in')
+    submit=SubmitField('Enter')
